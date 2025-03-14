d0ba3adab92356b1de4a22bd9d88b2ca05d1b51d
DorianBajorek <dorianb324@gmail.com>
2025-03-14 22:08:05 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,24 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-CW9JJSNE17"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CW9JJSNE17', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
