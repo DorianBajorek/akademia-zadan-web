83f19c29d4e2eb7eda725d50aa6fdf2939ab8865
DorianBajorek <dorianb324@gmail.com>
2025-07-13 20:35:41 +0200

    add changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,12 +7,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pl">
       <head>
-        {/* <Script
+        <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2838302537006263"
           crossOrigin="anonymous"
           strategy="afterInteractive"
-        /> */}
+        />
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-CW9JJSNE17"
